6c727d619cd8e54041c1b13369c4741bcd00a6a3
Wanvisa Suwannin <wanvisa.nun@gmail.com>
2019-04-24 17:11:41 +0700

    add signup

--- src/pages/signup/signup.ts
+++ src/pages/signup/signup.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+//import { MenuPage } from '../menu/menu';
+
+//HttpClient
+import { HttpClient } from '@angular/common/http';
+import { AlertController } from 'ionic-angular';
 import { MenuPage } from '../menu/menu';
-
-
 /**
  * Generated class for the SignupPage page.
  *
@@ -16,14 +19,42 @@ import { MenuPage } from '../menu/menu';
   templateUrl: 'signup.html',
 })
 export class SignupPage {
-
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  customer = {
+    customerID:"",
+    customerName:"",
+    customerTel:"",
+    customerMail:""
+  };
+  data:any;
+  constructor(public navCtrl: NavController, public navParams: NavParams,private http: HttpClient, private alertCtrl:AlertController) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad SignupPage');
   }
-  gotoMenu(){
-    this.navCtrl.push(MenuPage);
+ // gotoMenu(){
+  //  this.navCtrl.push(MenuPage);
+  //}
+  addCustomer(){
+    let url="http://localhost:8080/customer";
+    console.log(this.customer);
+    this.http.post(url, this.customer)
+      .subscribe(
+        res=>{
+            this.data = res;
+            if(this.data.msg==true){
+              this.showAlert("สำเร็จ","ข้อมูลถูกบันทึก");
+              this.navCtrl.push(MenuPage);
+            }
+        }
+      ); 
+  }
+  showAlert(msgTitle:string,message:string){
+    const alert = this.alertCtrl.create({
+      title: msgTitle,
+      subTitle: message,
+      buttons: ["OK"]
+    });
+    alert.present();
   }
 }
